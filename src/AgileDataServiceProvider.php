f552dfe82b0de21b4e278f0489d389d62b3e1ac5
FoxxMD <matt.duncan13@gmail.com>
2016-12-07 12:00:29 -0500

    Implement service provider and configuration
    * Default configuration point to Laravel's default db connection
    * Service Provider registers Persistence singleton with alias
    * Support Lumen

---
+++ src/AgileDataServiceProvider.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace atk4\LaravelAD;
+
+
+use atk4\data\Persistence;
+use FoxxMD\Utilities\ArrayUtil;
+use Illuminate\Support\ServiceProvider;
+
+class AgileDataServiceProvider extends ServiceProvider {
+	public function boot()
+	{
+		$this->publishes([
+			__DIR__ . '/agiledata.php' => config_path('agiledata.php'),
+		], 'agiledata');
+	}
+
+	public function register()
+	{
+		if ($this->isLumen()) {
+			$this->app->configure('database');
+			$this->app->configure('agiledata');
+		}
+
+		$this->registerPersistence();
+	}
+
+	protected function registerPersistence()
+	{
+		$this->app->singleton('agiledata', function ($app)
+		{
+			$config = $app->make('config');
+			if ('default' === $connectionName = $config->get('agiledata.connection'))
+			{
+				$connectionName = $config->get('database.default');
+			}
+			$connectionDetails = $config->get('database.connections.' . $connectionName);
+			$dsn               = "{$connectionDetails['driver']}:";
+			switch ($connectionDetails['driver'])
+			{
+				case 'mysql':
+					$dsn .= "host={$connectionDetails['host']};dbname={$connectionDetails['database']}";
+					break;
+				case 'sqlite':
+					$dsn .= "{$connectionDetails['database']}";
+			}
+
+			return Persistence::connect($dsn, ArrayUtil::get($connectionDetails['username']), ArrayUtil::get($connectionDetails['password']));
+		});
+
+		$this->app->alias('agiledata', Persistence::class);
+	}
+
+	/**
+	 * @return bool
+	 */
+	protected function isLumen()
+	{
+		return str_contains($this->app->version(), 'Lumen');
+	}
+}
