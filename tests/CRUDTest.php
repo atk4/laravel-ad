bf4d1d66e573fdfde691bb1f9219d64fe9751c48
Romans Malinovskis <romans@agiletoolkit.org>
2016-12-07 14:22:46 +0000

    intiilaized testsuite

---
+++ tests/CRUDTest.php
@@ -0,0 +1,72 @@
+<?php
+
+
+use atk4\data\Model;
+
+class User extends \atk4\data\Model {
+    public $table = 'user';
+
+    function init()
+    {
+        parent::init();
+
+        $this->addField('name');
+        $this->addField('surname');
+    }
+}
+
+/**
+ * Tests basic create, update and delete operatiotns
+ */
+class CRUDTest extends \atk4\schema\PHPUnit_SchemaTestCase
+{
+
+    public function testUpdate()
+    {
+
+        $q = [
+            'user' => [
+                ['name' => 'Vinny', 'surname' => 'Shira'],
+                ['name' => 'Zoe', 'surname' => 'Shatwell'],
+            ],
+        ];
+        $this->setDB($q);
+
+        $m = new User($this->db);
+
+        $m->tryLoadAny();
+        $m['name'] = 'Ken';
+        $m->save();
+
+        $m->load(2);
+        $m['name'] = 'Brett';
+        $m->save();
+        $m['name'] = 'Doug';
+        $m->save();
+
+        // TODO - add asserts
+    }
+
+    public function testAddDelete()
+    {
+
+        $q = [
+            'user' => [
+                ['name' => 'Jason', 'surname' => 'Dyck'],
+                ['name' => 'James', 'surname' => 'Knight'],
+            ],
+        ];
+        $this->setDB($q);
+        $zz = $this->getDB('user');
+
+        $m = new User($this->db);
+
+        //$m->getElement('surname')->default = 'Pen';
+
+        //$m->save(['name'=>'Robert']);
+
+        $m->loadBy('name', 'Jason');//->delete();
+
+        $this->assertEquals('Dyck', $m['surname']);
+    }
+}
---
+++ tests/DummyTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace atk4\LaravelAD\tests;
+
+class BasicTest extends \atk4\core\PHPUnit_AgileTestCase
+{
+    /**
+     * Test constructor.
+     */
+    public function testTesting()
+    {
+        $this->assertEquals('foo', 'foo');
+    }
+
+}
